8430077dded4382c3ae7d01d3f172da4958bcea0
wilianet <wilianet@gmail.com>
2024-08-16 21:53:33 -0300

    Automatic commit from Bubble

---
+++ elements/AAD-850m8/update.js
@@ -0,0 +1,12 @@
+function(instance, properties, context) {
+    // Encontra o elemento pelo ID fornecido nas propriedades
+    var inputElement = document.getElementById(properties.id_attribute);
+    
+    // Verifica se o elemento existe para evitar erros
+    if(inputElement) {
+        // Define o valor do campo para uma string vazia, que representa "nulo" para inputs
+        inputElement.value = '';
+    } else {
+        console.log("Elemento não encontrado com o ID: " + properties.id_attribute);
+    }
+}
